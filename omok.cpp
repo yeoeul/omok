7485b967431267222d4e5393f87d7e13afccf1ce
HyeSung <yeoeul.ruhe@gmail.com>
2020-11-22 20:33:51 +0900

    make class Player, thread setBoard, put_Stone

--- omok.cpp
+++ omok.cpp
@@ -10,11 +10,27 @@ public:
 	Board(){
 		for(int i = 0; i < 19; i++) for(int j = 0; j < 19; j++) m_board[i][j] = L'┼';
 	}
+	void setboard(int x, int y, int color);
 	void draw_Board();
 };
 
+void Board::setBoard(int x, int y, int color){
+	m_board[y][x] = color;
+}
+
 void Board::draw_Board(){
 	for(int i = 0; i < 19; i++) cout << m_board[i] << endl;
+}
+
+class Player{
+private:
+	
+public:
+	void put_Stone(int x, int y);
+}
+
+void Player::put_Stone(int x, int y, int color){
+	setBoard(x, y, color);
 }
 
 int main()
